42e643f02a420316013f8febd3b0ba26cf5bada8
Nicolas Cousin <niccoudev@gmail.com>
2019-02-05 21:21:01 +0100

    Initial commit

---
+++ WcfClient/WcfServiceAgent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WcfClient.WcfService;
+
+namespace WcfClient
+{
+    public class WcfServiceAgent
+    {
+        IWcfService wcfService;
+
+        public WcfServiceAgent(IWcfService wcfService)
+        {
+            this.wcfService = wcfService;
+        }
+
+        public string HitWCFService()
+        {
+            int val = 1;
+
+            string retVal = string.Empty;
+
+            retVal = this.wcfService.GetData(val);
+
+            return retVal;
+        }
+    }
+
+}
---
+++ WcfClientTest/WcfServiceAgentTest.cs
@@ -0,0 +1,64 @@
+﻿using WcfClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WcfClient.WcfService;
+using Moq;
+
+namespace WcfClientTest
+{
+    [TestClass()]
+    public class WcfClientUnitTest
+    {
+        [TestMethod()]
+        public void MockWcfService()
+        {
+            string val = "MockedValue";
+            string actualRetVal;
+
+            // Initialisation du Mock du service WCF
+            Mock<IWcfService> wcfMock = new Mock<IWcfService>();
+
+            // Initialisation de la fonction GetData(int)
+            wcfMock.Setup<string>(s => s.GetData(It.IsAny<int>())).Returns(val);
+
+            // Définition de l'objet Mock
+            IWcfService wcfMockObject = wcfMock.Object;
+
+            // Initialisation de l'agent avec le Mock du service
+            WcfServiceAgent serviceAgent = new WcfServiceAgent(wcfMockObject);
+
+            // Appel de la fonction
+            actualRetVal = serviceAgent.HitWCFService();
+
+            // Vérification des résultats
+            wcfMock.Verify(s => s.GetData(It.IsAny<int>()), Times.Exactly(1));
+            Assert.AreEqual("MockedValue", actualRetVal, "Not same.");
+        }
+
+        [TestMethod()]
+        public void MockWcfService2()
+        {
+            string val = "MockedValue";
+            string actualRetVal;
+
+            // Initialisation du Mock du service WCF
+            Mock<IWcfService> wcfMock = new Mock<IWcfService>();
+
+            // Initialisation de la fonction GetData(int)
+            wcfMock.Setup<string>(s => s.GetData(4)).Returns(val);
+
+            // Définition de l'objet Mock
+            IWcfService wcfMockObject = wcfMock.Object;
+
+            // Initialisation de l'agent avec le Mock du service
+            WcfServiceAgent serviceAgent = new WcfServiceAgent(wcfMockObject);
+
+            // Appel de la fonction
+            actualRetVal = serviceAgent.HitWCFService();
+
+            // Vérification des résultats
+            wcfMock.Verify(s => s.GetData(4), Times.Exactly(1));
+            Assert.AreEqual("MockedValue", actualRetVal, "Not same.");
+        }
+    }
+}
